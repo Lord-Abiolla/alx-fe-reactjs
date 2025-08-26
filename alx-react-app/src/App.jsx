7f54e0488d2382b06c8db935b2d53fa35eac30e7
Lord-Abiolla <abiollajames@protonmail.com>
2025-08-27 00:06:15 +0300

    Create UserProfile card using props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,9 @@
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
+import UserProfile from './components/UserProfile'
 import Footer from './components/Footer'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -14,7 +13,7 @@ function App() {
     <>
       <Header />
       <MainContent />
-      <WelcomeMessage />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
     </>
   )
