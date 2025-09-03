e2c046658599ff3ff826fe28fc85dee02a474244
Lord-Abiolla <abiollajames@protonmail.com>
2025-09-03 10:59:12 +0300

    Initial commit

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,23 @@
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import UserProfile from './components/UserProfile'
+import Footer from './components/Footer'
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <WelcomeMessage />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Footer />
+    </>
+  )
+}
+
+export default App
