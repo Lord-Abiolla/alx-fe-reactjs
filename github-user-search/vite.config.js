9348167538c980c2ab319f90b6170d565b8e4eee
Lord-Abiolla <abiollajames@protonmail.com>
2025-09-20 13:54:26 +0300

    Final fix for deployement

--- github-user-search/vite.config.js
+++ github-user-search/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || 'alx-fe-reactjs/tree/main/github-user-search',
   css: {
     postcss: './postcss.config.js',
   },
