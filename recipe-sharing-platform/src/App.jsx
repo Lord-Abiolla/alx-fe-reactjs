15e248cf32c89c047ac3b2084756d53bbb863875
Lord-Abiolla <abiollajames@protonmail.com>
2025-10-03 12:43:35 +0300

    Install react router dom and implement it on root of the project

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
+import RecipeDetail from './components/RecipeDetail';
 
 function App() {
 
   return (
     <div className='items-center justify-center p-10 bg-green-100 h-screen'>
-      <HomePage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/recipe/:id" element={<RecipeDetail />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
